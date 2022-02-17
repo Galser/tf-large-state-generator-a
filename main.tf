65febb1ad20f2836e075d5de995de142806be800
Andrii Guselietov <guseletov@gmail.com>
2022-02-17 12:22:11 +0100

    Randomize apply with keepers + var string length

--- main.tf
+++ main.tf
@@ -2,13 +2,21 @@
 resource "random_pet" "ourhoard" {
   count  = var.pets_count
   length = var.pet_words
+  keepers = {
+    # Generate a new pet name each time the time changes
+    make_on_every_apply = timestamp()
+  }
 }
 
 resource "random_string" "random" {
   count            = var.pets_count
-  length           = 1024
+  length           = var.string_length
   special          = true
   override_special = "/@£$"
+  keepers = {
+    # Generate a new string  each time the time changes
+    make_on_every_apply = timestamp()
+  }
 }
 
 #output "demo" {
