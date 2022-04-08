af90cb5eefe737221e064ecb145220a051191023
Andrii Guselietov <guseletov@gmail.com>
2022-04-08 17:14:11 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "pet_words" {
   description = "Number of words per pet"
 }
 variable "pets_count" {
-  default     = 50
+  default     = 500
   description = "Count of pets"
 }
 variable "string_length" {
